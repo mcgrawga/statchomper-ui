0cd4006965a103d9b5629b9c7e9650cf49c68e01
mcgrawga <garthmcgraw@statgolf.com>
2018-12-04 17:37:04 -0600

    Fixed a bug with links href=# going to top of page

--- script.js
+++ script.js
@@ -1,101 +1,4 @@
 $(document).ready(function(){
-    // const url = 'https://statchomper.herokuapp.com/basketball-statlines';
-    // $.ajax({
-    //     url,
-    //     type: "GET",
-    //     crossDomain: true,
-    //     success: function (response) {
-    //         for (let i = 0; i < response.length; i++){
-    //             const bs = response[i].boxScore.game;
-    //             $('body').append(`<table id="${response[i]._id}"></table>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id}">
-    //                 <td><a href="#" class="player">${response[i].player}</a></td><td><a href="#" class="player">${response[i].datePlayed}</a></td>
-    //             <td></td></tr>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Points: </td><td>${bs.points}</td></tr>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>2 point attempts: </td><td>${bs.twoPointAttempts}</td></tr>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>2 point made: </td><td>${bs.twoPointMade}</td></tr>`);
-    //             if (bs.twoPointPercentage === 'n/a' || bs.twoPointPercentage === 'NaN' || bs.twoPointPercentage === null){
-    //                 $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>2 point percent: </td><td>n/a</td></tr>`);
-    //             }else{
-    //                 $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>2 point percent: </td><td>${bs.twoPointPercentage}%</td></tr>`);
-    //             }
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>3 point attempts: </td><td>${bs.threePointAttempts}</td></tr>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>3 point made: </td><td>${bs.threePointMade}</td></tr>`);
-    //             if (bs.threePointPercentage === 'n/a' || bs.threePointPercentage === 'NaN' || bs.threePointPercentage === null){
-    //                 $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>3 point percent: </td><td>n/a</td></tr>`);
-    //             }else{
-    //                 $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>3 point percent: </td><td>${bs.threePointPercentage}%</td></tr>`);
-    //             }
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Free throw attempts: </td><td>${bs.freeThrowAttempts}</td></tr>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Free throw made: </td><td>${bs.freeThrowMade}</td></tr>`);
-    //             if (bs.freeThrowPercentage === 'n/a' || bs.freeThrowPercentage === 'NaN' || bs.freeThrowPercentage === null){
-    //                 $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Free throw percent: </td><td>n/a</td></tr>`);
-    //             }else{
-    //                 $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Free throw percent: </td><td>${bs.freeThrowPercentage}%</td></tr>`);
-    //             }
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Blocks: </td><td>${bs.blocks}</td></tr>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Fouls: </td><td>${bs.fouls}</td></tr>`);
-    //             $(`#${response[i]._id}`).append(`<tr class="${response[i]._id} stat"><td></td><td>Steals: </td><td>${bs.steals}</td></tr>`);
-    //         }
-    //     },
-    //     error: function (xhr, status) {
-    //         $('body').append(`${status}`);
-    //     }
-    // });
-
-    // const url = 'https://statchomper.herokuapp.com/basketball-statlines?sort=player';
-    // $.ajax({
-    //     url,
-    //     type: "GET",
-    //     crossDomain: true,
-    //     success: function (response) {
-    //         let curPlayer;
-    //         for (let i = 0; i < response.length; i++){
-    //             const bs = response[i].boxScore.game;
-    //             if (curPlayer !== response[i].player){
-    //                 curPlayer = response[i].player;
-    //                 $('body').append(`<ul><li><a href="#" class="player_name" id="${response[i].player}">${response[i].player}</a></li></ul>`);    
-    //             }
-    //             let opponent = '???';
-    //             if (response[i].opponent){
-    //                 opponent = response[i].opponent;
-    //             }
-    //             let twoPointPercentage = 'n/a';
-    //             if (bs.twoPointPercentage !== 'n/a' && bs.twoPointPercentage !== 'NaN' && bs.twoPointPercentage !== null){
-    //                 twoPointPercentage = bs.twoPointPercentage;
-    //             }
-    //             let threePointPercentage = 'n/a';
-    //             if (bs.threePointPercentage !== 'n/a' && bs.threePointPercentage !== 'NaN' && bs.threePointPercentage !== null){
-    //                 threePointPercentage = bs.threePointPercentage;
-    //             }
-    //             let freeThrowPercentage = 'n/a';
-    //             if (bs.freeThrowPercentage !== 'n/a' && bs.freeThrowPercentage !== 'NaN' && bs.freeThrowPercentage !== null){
-    //                 freeThrowPercentage = bs.freeThrowPercentage;
-    //             }
-    //             $(`#${curPlayer}`).append(`<ul class="games_${response[i].player}"><li>
-    //                 <a href="#">${response[i].datePlayed} vs. ${opponent}</a>
-    //                 <ul class="game_details ${response[i]._id}">
-    //                     <li>Points: ${bs.points}</li>
-    //                     <li>2 point attempts:: ${bs.twoPointAttempts}</li>
-    //                     <li>2 point made:: ${bs.twoPointMade}</li>
-    //                     <li>2 point percent:: ${twoPointPercentage}</li>
-    //                     <li>3 point attempts:: ${bs.threePointAttempts}</li>
-    //                     <li>3 point made:: ${bs.threePointMade}</li>
-    //                     <li>3 point percent:: ${threePointPercentage}</li>
-    //                     <li>Free throw attempts: ${bs.freeThrowAttempts}</li>
-    //                     <li>Free throw made: ${bs.freeThrowMade}</li>
-    //                     <li>Free throw percent: ${freeThrowPercentage}</li>
-    //                     <li>Blocks: ${bs.blocks}</li>
-    //                     <li>Fouls: ${bs.fouls}</li>
-    //                     <li>Steals: ${bs.steals}</li>
-    //                 </ul>
-    //             </li></ul>`);    
-    //         }
-    //     },
-    //     error: function (xhr, status) {
-    //         $('body').append(`${status}`);
-    //     }
-
     const url = 'https://statchomper.herokuapp.com/basketball-statlines?sort=player';
     $.ajax({
         url,
@@ -166,5 +69,6 @@ $(document).ready(function(){
         }
 
         $(`.${this.id}`).toggle();
+        e.preventDefault();
     });
 });
